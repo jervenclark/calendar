1ca459532d82dac5f291d5d5b293bc2809013ebc
Jerven Clark Chua <jervenclark.chua@gmail.com>
2020-05-08 17:56:29 +0800

    Add alphanumeric + spaces rule for validation

---
+++ app/Rules/AlphaNumericSpaces.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class AlphaNumericSpaces implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $pattern = '/(^[a-zA-Z0-9 ]+$)+/';
+        return preg_match($pattern, $value);
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'The :attribute format is invalid.';
+    }
+}
